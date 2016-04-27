c5659fe1acf82cd9cc98d9b5dca7a4ae5ddb78f3
Grounds, Bryan <bgrounds@ea.com>
2016-04-26 22:56:18 -0700

    add * origin to cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@
    */
   var app = express();
   app.use(errorHandler());
-  app.use(cors());
+  app.use(cors({origin:'*'}));
 
   /*****************************************************************************
    * configure http server
